1d04ab2480036a5c4d1a233a2252d0da3cf99179
Urjeet Patel <urjeet@gmail.com>
2021-06-03 22:27:40 -0500

    add handwash instructions

--- src/components/HowTo.svelte
+++ src/components/HowTo.svelte
@@ -1,1 +1,23 @@
-<h1>HowTo Component</h1>
+<div bp="grid">
+    <img
+        src="images/handwash.jpg"
+        alt="How to wash hands"
+        bp="offset-4@md 6@md 12@sm"
+    />
+</div>
+<h3>
+    <a
+        href="https://www.who.int/teams/integrated-health-services/infection-prevention-control"
+        >Image Source</a
+    >
+</h3>
+
+<style>
+    img {
+        max-width: 100%;
+    }
+    h3 {
+        font-size: x-small;
+        text-align: center;
+    }
+</style>
